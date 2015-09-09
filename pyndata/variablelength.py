632aeadc090cc5f34c6b8a0e285c453f55d5e123
Colin Valliant <alcarithemad@gmail.com>
2015-09-09 13:59:08 -0700

    Improved variable-length stuff

--- pyndata/variablelength.py
+++ pyndata/variablelength.py
@@ -1,10 +1,13 @@
 from __future__ import absolute_import
 
 from .field import Field
+from .bitfield import BitField
 
 class VariableLength(object):
     def get_length(self, struct):
             if isinstance(self.length, Field):
                 return struct.field_items[self.length.name]
+            elif isinstance(self.length, BitField):
+                return self.length.__get__(struct)
             else:
                 return self.length
