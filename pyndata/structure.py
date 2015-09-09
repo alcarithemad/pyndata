4903aa6765f35f3e9d7c2b470bff59cac87d1401
Colin Valliant <alcarithemad@gmail.com>
2015-09-09 13:55:24 -0700

    Better support for bitfields

--- pyndata/structure.py
+++ pyndata/structure.py
@@ -9,16 +9,25 @@ except:
 
 from .field import __nextfield__
 from .field import Field
+from .bitfield import BitField
 from .structfield import StructField
 
-# TODO: endianness
-
+# This is a little strange, so it deserves an explanation.
+# In StructMeta down below, we need to check if the newly created class is
+# a Struct, but the real Struct class doesn't exist yet, and won't exist
+# when it's being run through the __new__ method.
+#
+# Instead, we "forward declare" Struct here a la C, then inherit from this
+# Struct in the real Struct class below. If it helps, mentally replace every
+# "Struct" before "class Struct(object" (and the on in the base classes there)
+# with "FirstStruct".
 class Struct:
     pass
 
 class StructMeta(type):
     def __new__(cls, cls_name, bases, attrs):
         fields = []
+        bitfields = []
         field_defaults = {}
         for name, field in attrs.items():
             if issubclass(type(field), Field):
@@ -27,6 +36,9 @@ class StructMeta(type):
                     field.__SHOW__ = False
                 field_defaults[name] = field.default
                 fields.append(field)
+            elif issubclass(type(field), BitField):
+                field.name = name
+                bitfields.append(field)
             elif issubclass(type(field), Struct):
                 sf = StructField(field)
                 sf.name = name
@@ -39,6 +51,7 @@ class StructMeta(type):
         new_cls = type.__new__(cls, cls_name, bases, attrs)
         new_cls.field_defaults = field_defaults
         new_cls.__FIELDS__ = fields
+        new_cls.bitfields = bitfields
         return new_cls
 
 class Struct(object, Struct):
