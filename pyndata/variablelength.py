e4b6a7a3d90cbaa313deeaa4f6421c08368f3fee
Colin Valliant <alcarithemad@gmail.com>
2015-10-16 17:32:12 -0700

    Simplify VariableLength length field access.

--- pyndata/variablelength.py
+++ pyndata/variablelength.py
@@ -5,9 +5,7 @@ from .bitfield import BitField
 
 class VariableLength(object):
     def get_length(self, struct):
-            if isinstance(self.length, Field):
-                return struct.field_items[self.length.name]
-            elif isinstance(self.length, BitField):
+            if isinstance(self.length, Field) or isinstance(self.length, BitField):
                 return self.length.__get__(struct)
             else:
                 return self.length
