2497ec7c64a23ca57aab1ededcdbc57dd22f7182
Colin Valliant <alcarithemad@gmail.com>
2015-10-16 17:31:11 -0700

    Add/update tests.

--- tests/test_substruct.py
+++ tests/test_substruct.py
@@ -6,15 +6,18 @@ class SubStruct(pyndata.Struct):
 class StructWithSubStruct(pyndata.Struct):
     s1 = SubStruct()
     s2 = SubStruct()
+    _s3 = SubStruct()
 
 def test_sub_struct_unpack():
-    t = StructWithSubStruct('\xff\xcc')
-    assert t.s1.x == 0xff
-    assert t.s2.x == 0xcc
+    t = StructWithSubStruct('\x01\x23\x45')
+    assert t.s1.x == 0x01
+    assert t.s2.x == 0x23
+    assert t._s3.x == 0x45
 
 def test_sub_struct_pack():
     t = StructWithSubStruct()
     t.s1.x = 0x20
     t.s2.x = 0x44
+    t._s3.x = 0x59
     packed = t.pack()
-    assert packed == '\x20\x44'
+    assert packed == '\x20\x44\x59'
