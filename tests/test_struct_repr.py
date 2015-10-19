ecee70fa2155f6391e5049d730eb3c506f53c9cd
Colin Valliant <alcarithemad@gmail.com>
2015-10-19 14:48:45 -0700

    More struct repr testing.

--- tests/test_struct_repr.py
+++ tests/test_struct_repr.py
@@ -3,10 +3,12 @@ import pytest
 import pyndata
 
 class S(pyndata.Struct):
-	_a = pyndata.uint8()
-	b = pyndata.uint8()
+    _a = pyndata.uint8()
+    b = pyndata.uint8()
+    c = pyndata.BitField(b, 1)
+    _d = pyndata.BitField(b, 1)
 
 def test_default_hidden():
-	x = S()
-	a = repr(x)
-	assert a == "S(b=0)"
+    x = S()
+    a = repr(x)
+    assert a == "S(b=0, c=0)"
