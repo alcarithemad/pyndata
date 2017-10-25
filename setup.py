67586f95a74c72ff1f8796ccc213f660bdaee110
Colin Valliant <alcarithemad@gmail.com>
2017-10-24 21:46:57 -0700

    Minimal setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pyndata',
+    version='0.1',
+    packages=['pyndata'],
+    url='https://github.com/alcarithemad/pyndata',
+    license='2-clause BSD',
+    author='Colin Valliant',
+    author_email='alcarithemad@gmail.com',
+    description='concisely describe and process packed binary data'
+)
